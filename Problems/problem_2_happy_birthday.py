a566c989715863fa13e59b24bdcbef87815704a8
Destiny <destiny.lawver@icloud.com>
2026-03-11 16:41:20 -0400

    Personal Fork

--- Problems/problem_2_happy_birthday.py
+++ Problems/problem_2_happy_birthday.py
@@ -1,3 +1,8 @@
 """
 Ask for the user's name and then sing happy birthday to them.
 """
+name = input("What is your name?")
+print("Happy birthday to you!")
+print("Happy birthday to you!")
+print("Happy birthday dear " + name + "!")
+print("Happy birthday to you!")
--- Problems/problem_3_temperature_converter.py
+++ Problems/problem_3_temperature_converter.py
@@ -2,7 +2,14 @@
 Given a temperature in Fahrenheit, return the temperature in Celsius
 """
 # Ask for a temperature in Fahrenheit
+temp_f= input("What is the temperature in F:?")
+temp_f = int(temp_f)
 
 # Calculate it in Celsius
+print(temp_f)
+
+Temp_c = (int(temp_f) - 32) * 5/9
+Temp_c = str(Temp_c)
 
 # Tell the user what it is
+print("The temperature in C: " + Temp_c)
