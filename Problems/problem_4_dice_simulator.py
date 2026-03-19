4ef48e9f464950db83fee1ccffe039aeb7b4e27f
Destiny <destiny.lawver@icloud.com>
2026-03-19 19:04:36 -0400

    Finished Problem 4 and Problem 5

--- Problems/problem_4_dice_simulator.py
+++ Problems/problem_4_dice_simulator.py
@@ -1,7 +1,9 @@
 """
 Randomly returns two numbers between 1 and 6
 """
-
+import random
 # Generate two random integer between 1 and 6 (inclusive)
-
+number1 = random.randint(1, 6)
+number2 = random.randint(1, 6)
 # Tell the user what the result was
+print(f"You rolled a {number1} and a {number2}.")
--- Problems/problem_5_circle_stats.py
+++ Problems/problem_5_circle_stats.py
@@ -2,17 +2,22 @@
 Fill out the functions to calculate the area and circumference of a circle.
 Print the result to the user.
 """
-
+import math
 
 def area(r):
-    pass
+    return math.pi * r**2
 
 
 def circumference(r):
-    pass
+    return math.pi * 2 * r
 
 
-radius = input("Circle radius: ")
+radius = float(input("Circle radius: "))
 
-print('Area: {}')  # <-- Call the area function and print the result
-print('Circumference: {}')  # <-- Call the circumference function and print
+circle_area = area(radius)
+circle_cumference = circumference(radius)
+print(circle_area)
+print(circle_cumference)
+
+print('Area: ' + str(circle_area))  # <-- Call the area function and print the result
+print('Circumference: ' + str(circumference(radius)))  # <-- Call the circumference function and print
